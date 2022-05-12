3d3b207742a2cf623beef0bf4052540c89f06371
Seb <Sebastianmuchui79@gmail.com>
2022-05-12 15:20:28 +0300

    started

---
+++ init.rb
@@ -0,0 +1,42 @@
+
+
+# Strings
+
+@string = "string"
+puts @string
+
+# Concatenation
+print "this is a #{@string}"
+
+# MULTI LINE COMMENTS
+=begin
+   This is a
+   multi-line comment
+   
+=end
+
+# Conditionals
+
+if 2>3 then
+   puts "True"
+else 
+   puts "false"
+end
+
+# User Input
+
+@x = gets
+print @x
+
+
+
+
+# OOP
+class Hello
+   def initialize(name)
+         @name = name.capitalize
+   end
+   def salute
+      print "Hello #{@name}"
+   end
+end
